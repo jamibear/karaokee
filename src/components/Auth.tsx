bb2919e0501043e5b5e9bfd03410aeb8314c96b1
jamibear <5701ja.mi@gmail.com>
2023-06-12 00:21:44 +0800

    update redirect uri to deployed site

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,18 +1,20 @@
+import { REDIRECT_URI } from "../util/constants";
+
 const Auth = (): JSX.Element => {
-  const url = `https://accounts.spotify.com/authorize`;
-  const clientId = "8236c4f4f75b468f8b61a89d27ecd89c";
-  const state = "solamipasol";
-  const scope = "user-read-private%20user-read-email%20user-top-read";
-  const redirectUri = "http://localhost:3000/callback";
-  const showDialogue = "true";
+    const url = `https://accounts.spotify.com/authorize`;
+    const clientId = "8236c4f4f75b468f8b61a89d27ecd89c";
+    const state = "solamipasol";
+    const scope = "user-read-private%20user-read-email%20user-top-read";
+    const redirectUri = REDIRECT_URI;
+    const showDialogue = "true";
 
-  const authUrl = `${url}/?client_id=${clientId}&response_type=code&state=${state}&scope=${scope}&redirect_uri=${redirectUri}&show_dialog=${showDialogue}`;
+    const authUrl = `${url}/?client_id=${clientId}&response_type=code&state=${state}&scope=${scope}&redirect_uri=${redirectUri}&show_dialog=${showDialogue}`;
 
-  return (
-    <button>
-      <a href={authUrl}>Spotify</a>
-    </button>
-  );
+    return (
+        <button>
+            <a href={authUrl}>Spotify</a>
+        </button>
+    );
 };
 
 export default Auth;
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,8 @@
 export const SPOTIFY_TRACKS_ENDPOINT =
-  "https://api.spotify.com/v1/me/top/tracks?limit=50";
+    "https://api.spotify.com/v1/me/top/tracks?limit=50";
 export const SPOTIFY_CLIENT_ID = import.meta.env.SPOTIFY_CLIENT_ID ?? "";
 export const SPOTIFY_CLIENT_SECRET =
-  import.meta.env.SPOTIFY_CLIENT_SECRET ?? "";
+    import.meta.env.SPOTIFY_CLIENT_SECRET ?? "";
+
+export const REDIRECT_URI = "https://karaokee.netlify.app/callback";
+export const KARAOKEE_URL = "https://karaokee.netlify.app/";
